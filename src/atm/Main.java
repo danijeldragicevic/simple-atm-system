62b037c9a7b97e6b2f04b5c16ef89cb6d227dd19
Danijel Dragicevic <077226@canda.com>
2022-09-09 16:07:10 +0200

    Remove condition for custom database name. Don't depend on that anymore.

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -7,10 +7,6 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 0 && Objects.equals(args[0], "-fileName")) {
-            DBUtils.filename = args[1];
-        }
-
         CardController cardController = new CardController();
         cardController.start();
     }
